a22d9033a26b652615ec53f3ffee62e8dbf24ee1
Will <pgw1315@163.com>
2026-06-03 18:33:24 +0800

    删除没用的文件

--- fix_entity_names.ps1
+++
@@ -1,9 +0,0 @@
-$files = Get-ChildItem -Path . -Include *.cs -Recurse
-
-foreach ($file in $files) {
-    $content = Get-Content $file.FullName -Raw
-    if ($content -match "DeviceEntity") {
-        $content = $content -replace "DeviceEntity", "Device"
-        $content | Set-Content $file.FullName
-    }
-}
